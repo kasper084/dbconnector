3286a8bd01ad457aa7072e0fef3222305f6d47b9
kasper084 <kasper084@gmail.com>
2020-02-05 14:23:38 +0200

    start

---
+++ src/main/java/dbconnector/App.java
@@ -0,0 +1,13 @@
+package dbconnector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
